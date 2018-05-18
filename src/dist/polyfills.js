a0db637e96e0f9e19858e23d3862858f7279a981
andycoderw <andchir@gmail.com>
2018-05-18 17:15:00 +0300

    webpack setup

---
+++ src/dist/polyfills.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+require("core-js/es6");
+require("core-js/es7/reflect");
+require('zone.js/dist/zone');
+if (process.env.ENV === 'production') {
+    // Production
+}
+else {
+    // Development and test
+    Error['stackTraceLimit'] = Infinity;
+    require('zone.js/dist/long-stack-trace-zone');
+}
